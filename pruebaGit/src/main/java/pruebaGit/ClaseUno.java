88aa92ec704b18b5dc1d65af95307f94dc4e3434
Alejandro <Alejandro@Alejandro>
2016-09-08 15:54:48 -0500

    Primero Prueba

---
+++ pruebaGit/src/main/java/pruebaGit/ClaseUno.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package pruebaGit;
+
+/**
+ * @author Alejandro Matallana Caballero<br/>
+ *         email: alejito.matallana@gmail.com<br/>
+ *         Fecha: 8/09/2016<br/>
+ */
+public class ClaseUno {
+
+	private String hola;
+	
+	private String comoEsta;
+}
